ae2bcb1761e9d419c5f0467dccb8500d23c968cc
이정우 <jwlee2217@gmail.com>
2018-09-26 20:59:51 +0900

    passed alarm-priority

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -239,6 +239,7 @@ thread_unblock (struct thread *t)
   ASSERT (t->status == THREAD_BLOCKED);
   list_push_back (&ready_list, &t->elem);
   t->status = THREAD_READY;
+  list_sort(&ready_list, priority_asc, NULL);
   intr_set_level (old_level);
 }
 
